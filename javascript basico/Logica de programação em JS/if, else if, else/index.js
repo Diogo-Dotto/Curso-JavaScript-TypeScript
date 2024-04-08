0e1dbd2f24be9ead47d283c2535b16d8ce1de877
Diogo Fernando Dotto <158113109+Diogo-Dotto@users.noreply.github.com>
2024-04-08 00:10:57 -0300

    att

---
+++ javascript basico/Logica de programação em JS/if, else if, else/index.js
@@ -0,0 +1,19 @@
+//const hora = 50;
+
+//if (hora >= 0 && hora <= 12) {
+//    console.log(`Bom dia!`);
+//} else if (hora <= 17) {
+//    console.log(`Boa tarde!`);
+//} else if (hora <= 23) {
+//    console.log(`Boa noite!`);
+//} else {
+//    console.log(`Olá, horário não existe!`);
+//}
+
+const tenhoDinheiro = false;
+
+if (tenhoDinheiro) {
+    console.log(`Vou dar uma volta!`);
+} else {
+    console.log(`Vou ficar em casa!`);
+}
