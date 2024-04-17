67eaa1c72f7a9d1c0a43bd3899f830786c5c5241
Diogo Fernando Dotto <158113109+Diogo-Dotto@users.noreply.github.com>
2024-04-16 22:58:43 -0300

    att

--- Logica de programação em JS/estrutura de repetição/for/exercicio for/modelo html/assets/js/main.js
+++ Logica de programação em JS/estrutura de repetição/for/exercicio for/modelo html/assets/js/main.js
@@ -1,5 +1,5 @@
 const elementos = [
-    {tag: 'p', texto: 'Frase 1'},
+    {tag: 'p', texto: 'Qualquer texto'},
     {tag: 'div', texto: 'Frase 2'},
     {tag: 'footer', texto: 'Frase 3'},
     {tag: 'section', texto: 'Frase 4'},
