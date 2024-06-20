5f422932aaa2545edd95dae1106fb4310be89559
Diogo Fernando Dotto <diogodotto@hotmail.com>
2024-06-19 23:41:13 -0300

    Conectando MongoDB

---
+++ Node/13 - Express Middlewares/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const routes = require('./routes');
+const path = require('path');
+const { middlewareGloblal } = require('./src/middlewares/middleware')
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+app.set('views', path.resolve(__dirname, 'src', 'views'));
+app.set('view engine', 'ejs');
+
+//Nosso próprios middlewares
+app.use(middlewareGloblal);
+app.use(routes);
+
+app.listen(3000, () => {
+  console.log('Acessar http://localhost:3000');
+  console.log('Servidor executando na porta 3000');
+});
